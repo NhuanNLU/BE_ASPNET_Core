86a4ed4f68f9105b1ec6b3efbcf46586b25efd63
Nhuan <nhuan.study.net@gmail.com>
2026-04-15 16:24:38 +0700

    new commit

--- BE.ConsoleApp/BE.NetCoreAPI/LyThuyet.cs
+++ BE.ConsoleApp/BE.NetCoreAPI/LyThuyet.cs
@@ -3,7 +3,14 @@
     public class LyThuyet
     {
         /*
-         
+        http protocol để gửi nhận dữ liệu giữa client và server
+        dữ liệu ở đây có thể là json, xml, text, html, v.v... nhưng phổ biến nhất là json
+        http và https khác nhau gì?
+         - http là giao thức truyền tải dữ liệu không được mã hóa, 
+            trong khi https là giao thức truyền tải dữ liệu được mã hóa bằng SSL/TLS. 
+            Điều này có nghĩa là khi sử dụng https, 
+            dữ liệu giữa client và server sẽ được bảo vệ khỏi việc bị nghe lén hoặc thay đổi bởi bên thứ ba.
          */
     }
 }
+  
