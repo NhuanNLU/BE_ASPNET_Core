7567d946fd0221c40b8fb21eacfbb2dc69903610
Nhuan <nhuan.study.net@gmail.com>
2026-04-17 22:11:02 +0700

    new commit

--- BE.ConsoleApp/BE.NetCoreAPI/LyThuyet.cs
+++ BE.ConsoleApp/BE.NetCoreAPI/LyThuyet.cs
@@ -10,6 +10,17 @@
             trong khi https là giao thức truyền tải dữ liệu được mã hóa bằng SSL/TLS. 
             Điều này có nghĩa là khi sử dụng https, 
             dữ liệu giữa client và server sẽ được bảo vệ khỏi việc bị nghe lén hoặc thay đổi bởi bên thứ ba.
+        client giữ public key, server giữ private key,
+        Khi dữ liệu được gửi từ client đến server, nó sẽ dùng private key và public key để mã hóa dữ liệu,
+        Nên khi hacker có lấy được dữ liệu nó không có private key để giải mã dữ liệu đó
+        ngoài mã hóa password còn có thể mã hóa stk 
+         */
+
+        /*
+        Middleware là một model code có nhận Request và trả Response
+        Bản chất nó chỉ là các class Dùng để check Request
+        Nhận Http Response, sửa nó và chuyển đến Middleware khác
+        Các Middleware được gọi theo thứ tự trong pipeline
          */
     }
 }
